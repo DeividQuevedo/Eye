985832a2262bb4aace7265004f441e7288171379
DeividQuevedo <deivid.lejes@gmail.com>
2024-07-02 18:12:17 -0300

    Improve stability and error handling in Flask app & add eye_detection

--- camera/camera.py
+++ camera/camera.py
@@ -1,17 +1,22 @@
 import cv2
 from detection.facial_landmarks import detect_landmarks
+from detection.eye_detection import detect_eyes_and_calculate_ear
 
 class Camera:
     def __init__(self):
         self.video = cv2.VideoCapture(0)
+        if not self.video.isOpened():
+            raise Exception("Could not open video device")
 
     def __del__(self):
-        self.video.release()
+        if self.video.isOpened():
+            self.video.release()
 
     def get_frame(self):
         success, frame = self.video.read()
         if success:
-            frame = detect_landmarks(frame)
+            # Aplicar detecção de landmarks e cálculo de EAR
+            frame = detect_eyes_and_calculate_ear(frame)
             ret, jpeg = cv2.imencode('.jpg', frame)
             return jpeg.tobytes()
         else:
